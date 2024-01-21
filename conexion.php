ab7eaab317e1a2544eb40e97551b68da7a5f0566
Alexandro Delgado <82964108+adelgadoj@users.noreply.github.com>
2024-01-21 08:09:44 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli("localhost", "user1", "123456", "mochilas" , "3307");  
+    $conexion = new mysqli("localhost", "user", "password", "name_bd" , "3307");  
     $conexion->set_charset("utf8");
 ?>
